91288e52d1b227cbdd3a589daadb89358bcfb174
yama864 <yama864ao517@icloud.com>
2021-10-12 21:33:10 +0900

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   get 'prototypes/index', to:'prototypes#index'
+
   root to: "prototypes#index"
+
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: :create
+  end
+
+  resources :users, only: :show
+  
 end
